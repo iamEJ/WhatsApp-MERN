781ddcc6f7852414065dc111041c4ee6b013c694
iamEJ <jukneviciute185@gmail.com>
2020-10-01 15:54:48 +0300

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC2QzRtzkkWhbOP3DYOAu7QZnfKwwd_ibs",
+  authDomain: "whatsapp-mern-9c9ae.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-9c9ae.firebaseio.com",
+  projectId: "whatsapp-mern-9c9ae",
+  storageBucket: "whatsapp-mern-9c9ae.appspot.com",
+  messagingSenderId: "389361441208",
+  appId: "1:389361441208:web:ec84f8ae5037dd974d174d",
+};
