b5465d6c9753a5504418dbdf2c8cb6cc991dbcf5
iamEJ <jukneviciute185@gmail.com>
2020-10-01 17:41:26 +0300

    add sidebar component and style it

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./css/Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { IconButton, Avatar } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar_header">
+        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg/1200px-Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg" />
+        <div className="sidebar_header_right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar_search">
+        <div className="sidebar_search_container">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Find or add new chat" />
+        </div>
+      </div>
+      <div className="sidebar_chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
     </div>
   );
 }
